d16ba6a552de4cff2941e689d67a5216b9ca73de
stepash <spshoup@gmail.com>
2013-10-24 20:54:41 +1100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
- # get "static_pages#home"
-  
+  resources :users
   root 'static_pages#home'
   match '/signup',  to:  'users#new',             via:  'get'  
   match '/help',    to:  'static_pages#help',     via:  'get'
